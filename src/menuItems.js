6a7ff03c2584839d69fdd2c9d6bf21ac53122008
Jorge Zozaya <jorge.zozaya.verdiguel@gmail.com>
2022-02-12 12:52:21 -0800

    first commit

---
+++ src/menuItems.js
@@ -0,0 +1,57 @@
+export const menuItems = [
+  {
+    title: "Home",
+  },
+  {
+    title: "Services",
+    submenu: [
+      {
+        title: "web design",
+      },
+      {
+        title: "web development",
+        submenu: [
+          {
+            title: "Frontend",
+            submenu: [
+              {
+                title: "React",
+              },
+              {
+                title: "Vue",
+              },
+            ],
+          },
+          {
+            title: "Backend",
+            submenu: [
+              {
+                title: "NodeJS",
+              },
+              {
+                title: "PHP",
+              },
+              {
+                title: "Python",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        title: "SEO",
+      },
+    ],
+  },
+  {
+    title: "About",
+    submenu: [
+      {
+        title: "Who we are",
+      },
+      {
+        title: "Our values",
+      },
+    ],
+  },
+];
